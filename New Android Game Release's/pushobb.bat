820be486a798f84495740417eebd76de043062db
Jedithegreat <84155438+Jedithegreat@users.noreply.github.com>
2024-05-10 12:35:07 -0400

    Add files via upload

---
+++ New Android Game Release's/pushobb.bat
@@ -0,0 +1,8 @@
+
+
+:: just change the folder name in the code below to the folder to push, then put both files into the ABD folder then run this bat file
+
+
+@echo off
+adb push com.disney.ducktalesremastered_goo /sdcard/Android/obb/
+pause
